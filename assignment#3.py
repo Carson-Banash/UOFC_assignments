26f43c7f447d8988ca5b6ce271690fd1dcdb25d7
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-03-02 13:30:10 -0700

    basic set up of functions

--- assignment#3.py
+++ assignment#3.py
@@ -1,4 +1,14 @@
 from SimpleGraphics import *
 
-def handleInput(parameter_list):
+
+def handleInput (parameter_list):
+    pass
+
+def blendPixel (parameter_list):
+    pass
+
+def combineAll (parameter_list):
+    pass
+
+def main (parameter_list):
     pass
