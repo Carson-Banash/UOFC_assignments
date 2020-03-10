5ac17971d0d9bbc51536425474b593adea231c42
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-03-10 15:19:59 -0600

    added the whole handleinput function

--- assignment#3.py
+++ assignment#3.py
@@ -1,8 +1,22 @@
 from SimpleGraphics import *
 
 
-def handleInput (parameter_list):
-    pass
+def handleInput ():
+    print("Choose a transform type from the following:")
+    print("1: Combine two whole images.")
+    print("2: Verticaly combine two images.")
+    print("3: Horizontaly combine two images.")
+
+    choice = int(input("What is your choice? "))
+
+    while choice != 1 and choice != 2 and choice != 3 :
+        print("please enter either 1,2, or 3. again your choices are:")
+        print("1: Combine two whole images.")
+        print("2: Verticaly combine two images.")
+        print("3: Horizontaly combine two images.")
+        choice = int(input("What is your choice? "))
+
+    return choice
 
 def blendPixel (parameter_list):
     pass
@@ -10,5 +24,7 @@ def blendPixel (parameter_list):
 def combineAll (parameter_list):
     pass
 
-def main (parameter_list):
-    pass
+def main ():
+    print("your choice is", handleInput())
+
+main()
