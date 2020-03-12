d2de3fd1cc923dab75dfe5bb64792096c97d73c9
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-03-12 13:01:08 -0600

    + to the comall fn,created comparts, issue for avg

--- assignment#3.py
+++ assignment#3.py
@@ -18,17 +18,39 @@ def handleInput ():
 
     return choice
 
-def blendPixel (parameter_list):
-    pass
+def blendPixel (image_1, image_2, x, y):
+    r_img1, g_img1, b_img1 = getPixel(image_1, x, y)
+    r_img2, g_img2, b_img2 = getPixel(image_2, x, y)
 
-def combineAll (parameter_list):
+    r_avg = r_img1 + r_img2 / 2
+    g_avg = g_img1 + g_img2 / 2
+    b_avg = b_img1 + b_img2 / 2
+    
+    return r_avg, g_avg, b_avg
+
+def combineAll (image_1, image_2):
+    allBlended = createImage(getWidth(image_1), getHeight(image_1))
+    for y in range(0, getHeight(image_1)):
+            for x in range(0, getWidth(image_1)):
+                r,g,b = blendPixel(image_1, image_2, x, y)
+                print(r,g,b)
+                putPixel(allBlended, x, y, r, g, b)
+    return(allBlended)
+
+def combineParts():
     pass
 
 def main ():
+    image_1 = loadImage("/Users/carson/Desktop/DATA 211/Assignments/Mars.gif")
+    image_2 = loadImage("/Users/carson/Desktop/DATA 211/Assignments/Water.gif") 
+    
+
     choice = handleInput()
 
     if choice == 1:
-        pass
+        
+        drawImage(combineAll(image_1, image_2), 0, 0)
+
     elif choice == 2:
         pass
     elif choice == 3:
