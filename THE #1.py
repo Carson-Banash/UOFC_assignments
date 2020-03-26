724b67f984ac5ca25b5585604c298e9ad51d6c1b
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-03-25 22:06:34 -0600

    comments for the functions and their prmts/returns

--- THE #1.py
+++ THE #1.py
@@ -1,21 +1,39 @@
+#Carson Banash (30085955)
+#Lab B06
+
+#this funciton is the function that uses nested loops
+#this function determines if a given list is a sublist of another 
+#the parameters are:
+#Slist- this is the the smaller of the two list and the possible sublist
+#Llist- this is the larger of the list and the one that is compaired to
+#there is only one return, True or False, 
+#that is if the smaller list is a sublist of the larger one.
 def isAsublistV1(Slist, Llist):
-	isSublist = False
-	
-	if Slist == Llist:
-		isSublist = True
+    isSublist = False
+        
+    if Slist == Llist:
+        isSublist = True
 
-	else:
-		for i in range(0,len(Llist)):
-			if Llist[i] == Slist[0]:
-				n = 1
-				while (n < len(Slist)) and (Llist[i+n] == Slist[n]):
-					n += 1
-				
-				if n == len(Slist):
-					isSublist = True
+    else:
+        for i in range(0,len(Llist)):
+            if Llist[i] == Slist[0]:
+                n = 1
+                while (Llist[i+n] == Slist[n]) and (n < len(Slist)):
+                    n += 1
+                    
+                    if n == len(Slist):
+                        isSublist = True
 
-	return isSublist
+    return isSublist
 
+
+#this funciton is the function that uses a single loop
+#this function determines if a given list is a sublist of another 
+#the parameters are:
+#Slist- this is the the smaller of the two list and the possible sublist
+#Llist- this is the larger of the list and the one that is compaired to
+#there is only one return, True or False, 
+#that is if the smaller list is a sublist of the larger one.
 def isAsublistV2(Slist, Llist):
     n = 0
     isSublist = False
