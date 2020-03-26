c47449dbc7c55795a64ae00bfae61bfd5b0c0f45
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-03-25 22:17:19 -0600

    comments for the first variables

--- THE #1.py
+++ THE #1.py
@@ -4,50 +4,63 @@
 #this funciton is the function that uses nested loops
 #this function determines if a given list is a sublist of another 
 #the parameters are:
-#Slist- this is the the smaller of the two list and the possible sublist
-#Llist- this is the larger of the list and the one that is compaired to
+#sList- this is the the smaller of the two list and the possible sublist
+#lList- this is the larger of the list and the one that is compaired to
 #there is only one return, True or False, 
 #that is if the smaller list is a sublist of the larger one.
-def isAsublistV1(Slist, Llist):
+def isAsublistV1(sList, lList):
+    #sets the initial state of the isSublist to False
     isSublist = False
         
-    if Slist == Llist:
+    #says that if the list are identical then the smaller list is a sublist
+    if sList == lList:
         isSublist = True
 
+    #if they areent itedical then the program will exicute this code
     else:
-        for i in range(0,len(Llist)):
-            if Llist[i] == Slist[0]:
+        #runs through the numbers 0- the lenght of the larger list
+        for i in range(0,len(lList)):
+            #if the element of the larger list at i is equal to the element of the -
+            #smaller list at 0 then n is set to one and the rest of the code is exucted
+            if lList[i] == sList[0]:
                 n = 1
-                while (Llist[i+n] == Slist[n]) and (n < len(Slist)):
+                
+                #says that while the elements of the small list and the large list, 
+                #at their given indexes are the same and that the counter n is less than  -
+                #the length of the smaller list then the while loop will run
+                while (lList[i+n] == sList[n]) and (n < len(sList)):
                     n += 1
                     
-                    if n == len(Slist):
-                        isSublist = True
+                    #if the number of matched elemts is equal to the length of the small list -
+                    #then the small list is indeed a sublist of the larger
+                    if n == len(sList):
+                        isSublist = True #sets the value for isSublist to True
 
+    #retuns the boolean value of isSublist, either False or True
     return isSublist
 
 
 #this funciton is the function that uses a single loop
 #this function determines if a given list is a sublist of another 
 #the parameters are:
-#Slist- this is the the smaller of the two list and the possible sublist
-#Llist- this is the larger of the list and the one that is compaired to
+#sList- this is the the smaller of the two list and the possible sublist
+#lList- this is the larger of the list and the one that is compaired to
 #there is only one return, True or False, 
 #that is if the smaller list is a sublist of the larger one.
-def isAsublistV2(Slist, Llist):
+def isAsublistV2(sList, lList):
     n = 0
     isSublist = False
-    if Slist[0] in Llist:
-        overlap_start = Llist.index(Slist[0])
+    if sList[0] in lList:
+        overlap_start = lList.index(sList[0])
     else:
         isSublist = False
-    for i in range(0,len(Slist)):
-        if Slist[i] in Llist:
+    for i in range(0,len(sList)):
+        if sList[i] in lList:
             n += 1
             
-    possible_subList = Llist[overlap_start:(overlap_start+n)]
+    possible_subList = lList[overlap_start:(overlap_start+n)]
     
-    if possible_subList == Slist:
+    if possible_subList == sList:
         isSublist = True
 
     return isSublist
