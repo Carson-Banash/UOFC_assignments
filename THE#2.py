1e5c3b7a802ab1dcb7ec9f24cbd60d8428bf06e7
Carson-Banash <44104533+Carson-Banash@users.noreply.github.com>
2020-04-15 23:04:55 -0600

    comments for the whatCategory function

--- THE#2.py
+++ THE#2.py
@@ -38,13 +38,19 @@ def split_file(in_file):
         #sets the line to the next line in the file, this is needed so the file is looped through
         line = in_file.readline()
 
-    #the function returs the list of the file in lowercase and the original, each element being a line from the file
+#the function returs the list of the file in lowercase and the original, each element being a line from the file
     return file_list_lower, file_list_regular
 
-def whatType(string1, string2):
+#this function is responsible for adding each character of two given strings to its own dictinary.
+#however, it doesnt add punctiation or spaces to the dicinaries
+#there are two paramaters, they are both of the stings to be compaired
+#there is one return, that being a sting that either says the stings are part of categorie A of B 
+def whatCategory(string1, string2):
+    #sets phrase one and two to the two strings given to the function
     phrase_one = string1
     phrase_two = string2
-    is_type_A = False
+    #sets the initial boolean value of is_category_A to false
+    is_category_A = False
     
     #a list of characters that are not used in determining the category
     bad_characters = [".", ",", " ", "-", "?", "!"]
@@ -58,35 +64,55 @@ def whatType(string1, string2):
         #checks if the character is already in the phrase one dictinary and if its not in the 
         #bad characters list
         if char in dphrase_one and char not in bad_characters:
-            #if both evalueate to true, then the characters value is incremented by one
+            #if both statmets in the if statment evaluate to true, 
+            #then the characters value is incremented by one
             dphrase_one[char] += 1
-        #if none of the statments above evaluate to true then it will add the character
-        #to the dictinary with a value of one
+        #checks if the character is not alerady in the pharse one dictinary and if its not in the 
+        #bad characters list
         elif char not in dphrase_one and char not in bad_characters:
+            #if none of the statments above evaluate to true then it will add the character
+            #to the dictinary with a value of one
             dphrase_one[char] = 1
 
     #loops through all the characters in the second phrase
     for char in phrase_two:
-
+        #says that if the character is not in the dictinary of the first phrase
+        #the two phases are not in category A 
         if char not in dphrase_one:
-            is_type_A = False
+            is_category_A = False
+        #checks if the character is already in the phrase two dictinary and if its not in the 
+        #bad characters list
         elif char in dphrase_two and char not in bad_characters:
+            #if both statmets in the if statment evaluate to true, 
+            #then the characters value is incremented by one
             dphrase_two[char] += 1
+        #checks if the character is not alerady in the pharse one dictinary and if its not in the 
+        #bad characters list
         elif char not in dphrase_two and char not in bad_characters:
+            #if none of the statments above evaluate to true then it will add the character
+            #to the dictinary with a value of one
             dphrase_two[char] = 1
 
+    #says that if the two dictinarys are the same then the two phrases are in category A
     if dphrase_one == dphrase_two:
-        is_type_A = True
+        is_category_A = True
     
+    #says that if the two strings are the same then the two phrases are not in category A
+    #this is because they are in the same order which mean that they are category B
     if phrase_one == phrase_two:
-        is_type_A = False
+        is_category_A = False
     
-    if is_type_A == True:
-        type_statment = "Category A"
+    #says that if the boolean value for is_category_A is true then the two stings are in category A
+    if is_category_A == True:
+        #sets the category statment to A
+        category_statment = "Category A"
+    #if the boolean value for is_category_A is not true (false) then its category B
     else:
-        type_statment = "Category B"
+        #sets the category statment to B
+        category_statment = "Category B"
 
-    return type_statment
+    #returns the category statment, either A or B
+    return category_statment
         
 def main():
     input_file = open("THE2.in", "r")
@@ -94,7 +120,7 @@ def main():
 
     file_list_lower, file_list_regular = split_file(input_file)
     for i in range (0,len(file_list_lower),2):
-        output_file.write('"' + file_list_regular[i] + '"' + " and " + '"' + file_list_regular[i+1] + '": ' + whatType(file_list_lower[i], file_list_lower[i+1]) +"\n")
+        output_file.write('"' + file_list_regular[i] + '"' + " and " + '"' + file_list_regular[i+1] + '": ' + whatCategory(file_list_lower[i], file_list_lower[i+1]) +"\n")
 
 main()
 
